1481f3f2463da5733a094b424e236f83c4543226
HirenPattaniSCS <hiren.p.serpentcs@gmail.com>
2018-04-16 20:26:53 +0530

    [FIX]Fixed test file.

--- jasper_reports/tests/test_jasper_report.py
+++ jasper_reports/tests/test_jasper_report.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import base64
-import mock
 from odoo.tests.common import TransactionCase
 from odoo.modules.module import get_module_resource
 # from odoo.addons.jasper_reports.controllers.main import ReportController
@@ -11,6 +10,8 @@ class TestJasperReport(TransactionCase):
 
     def setUp(self):
         super(TestJasperReport, self).setUp()
+        # First we have to convert .jrxml file to binary
+        # In order to create a new report record
         # File path
         path = get_module_resource('jasper_reports', 'tests', 'user.jrxml')
         # Opening file
@@ -40,10 +41,9 @@ class TestJasperReport(TransactionCase):
         })
 
     def test_report(self):
-        with mock.patch('odoo.addons.jasper_reports.controllers.main.'
-                        'ReportController') as ReportController:
-            return ReportController.report_routes(
-                self,
-                reportname=self.report_data.report_name,
-                converter="jasper"
-                )
+        report_object = self.env['ir.actions.report']
+        report_name = self.report_data.report_name
+        report = report_object._get_report_from_name(report_name)
+        docs = self.env['res.users'].search([])
+        self.assertEqual(report.report_type, 'qweb-pdf')
+        return report.render_jasper(docs.ids, {})
